6f8b7ff4ac2657f4313435f8dc64b4f91de4f481
daryaav <36270322+daryaav@users.noreply.github.com>
2018-12-18 18:46:01 +0100

    Primer Commit

---
+++ app/src/main/java/volkodav/ampilogova/darya/projecte_github/MainActivity.java
@@ -0,0 +1,13 @@
+package volkodav.ampilogova.darya.projecte_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
